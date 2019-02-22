7061ccdcbd5291c7f989d0004bc10956bd662638
Alecx Moritz <alecxmoritz7@gmail.com>
2019-02-21 23:51:32 -0500

    added custom theme and minor theming

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule, MatButtonModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
